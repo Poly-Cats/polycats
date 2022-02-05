22c889b62689eeaf0dcf996dfa8d8f43956acc42
Poly Cats <99096245+Poly-Cats@users.noreply.github.com>
2022-02-05 12:48:39 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Work In progress, we appreciate your patience.
+          By Poly Cats Team</code>
         </p>
       </main>
 
